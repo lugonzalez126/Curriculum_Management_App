0e521363080dd1eca67b3e1702db2fc17ab8e6eb
lugonzalez126 <116767536+lugonzalez126@users.noreply.github.com>
2026-03-29 21:27:33 -0700

    Add Shelf frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,9 @@ app = FastAPI(title="ArkHive API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+    "curriculummanagementapp-production.up.railway.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
